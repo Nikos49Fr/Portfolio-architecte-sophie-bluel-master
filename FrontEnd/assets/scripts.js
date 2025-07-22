45fbd7c91363785704cb3617a189c29ac3c9f215
Nicolas Grignard <nicolas4980@gmail.com>
2025-07-22 12:33:53 +0200

    Correction : images bien importées depuis l'API

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -10,16 +10,10 @@ function updateGallery(works) {
     gallery.innerHTML = "";
     for (let work of works) {
         let figure = `<figure>
-				        <img src="${urlImgTranslate(work.imageUrl)}"
+				        <img src="${work.imageUrl}"
                          alt="${work.title}">
 				        <figcaption>${work.title}</figcaption>
 			        </figure>`
         gallery.innerHTML += figure;
     }
-}
-
-function urlImgTranslate(urlAPI) {
-    const regex = new RegExp("(/images/\\D+)");
-    let usableURL = regex.exec(urlAPI);
-    return "assets" + usableURL[0] + ".png";
 }
