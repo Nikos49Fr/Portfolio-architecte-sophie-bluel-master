f8f246538a63466e7e7041721f92432f619a93ea
Nicolas Grignard <nicolas4980@gmail.com>
2025-07-30 22:28:33 +0200

    Ajout de la fenêtre modale - Gestion de son apparition et disparition

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -7,26 +7,16 @@ const works = await reponseWorksAPI.json();
 const reponseCategoriesAPI = await fetch("http://localhost:5678/api/categories");
 const categories = await reponseCategoriesAPI.json();
 
-// génération des boutons de filtres dynamiquement
-addButtonsFilter(listingCategories(works));
-
-// affichage de la gallery
-updateGallery(works);
-
-// ajout des Listeners sur les boutons filtres
-filteredButtonsListener();
-
-// vérifie si l'utilisateur est authentifié
-isAdminConnected();
-
-
-
-/*
+/**************************************************
  * FONCTIONS
- */
+ **************************************************/
 
 /**************************************************
- * Retourne un set avec toutes les catégories, y compris "Tous"
+ * Génère l'ensemble des catégories présentes dans les travaux,
+ * incluant une catégorie "Tous" par défaut.
+ * 
+ * @param {Array} works - Liste des travaux à analyser
+ * @returns {Set} Ensemble des noms de catégories
  **************************************************/
 function listingCategories(works) {
 
@@ -38,27 +28,32 @@ function listingCategories(works) {
     }
     return caterogiesList;
 }
-
 /**************************************************
- * ajoute dynamiquement les bouton de filtres à la gallerie
+ * Ajoute dynamiquement les boutons de filtres à la galerie,
+ * en incluant un bouton "Tous" actif par défaut.
+ *
+ * @param {Set} categories - Ensemble des catégories à afficher
  **************************************************/
-function addButtonsFilter(categories) {
+function addFilterButtons(categories) {
     const filters = document.querySelector(".filters");
 
     for (const category of categories) {
             const button = document.createElement("button");
+            button.classList.add("btn", "btn-filters");
             button.dataset.category = category;
             
             // class .active par défaut au bouton "Tous"
-            category === "Tous" ? button.classList.add("active"): "";
+            category === "Tous" ? button.classList.add("active-btn"): "";
             
             button.innerText = category;
             filters.appendChild(button);
     }
 }
-
 /**************************************************
- * affiche les travaux, filtrés ou non
+ * Met à jour l'affichage de la galerie avec les travaux fournis,
+ * qu'ils soient filtrés ou non.
+ *
+ * @param {Array} works - Liste des travaux à afficher
  **************************************************/
 function updateGallery(works) {
     const gallery = document.querySelector(".gallery");
@@ -74,66 +69,196 @@ function updateGallery(works) {
         gallery.innerHTML += figure;
     }
 }
-
 /**************************************************
- * EventListener sur les bouttons filtre
+ * Ajoute les EventListeners sur les boutons de filtre,
+ * pour filtrer dynamiquement les travaux affichés.
  **************************************************/
 function filteredButtonsListener() {
     const buttons = document.querySelectorAll(".filters button");
-    
+
     for (const button of buttons) {
         button.addEventListener("click", function() {
-            
             const filteredWorks = works.filter(function (work) {
                 // retourne tous les éléments si la catégorie "Tous", sinon renvoi seulement les éléments dans la catégorie correspondant au bouton
                 return button.dataset.category === "Tous" ? true : work.category.name === button.dataset.category;
             });
-            
             // supprime la class "active" sur tous les bouttons
-            buttons.forEach(button => button.classList.remove("active"))
+            buttons.forEach(button => button.classList.remove("active-btn"));
             // remet la class "active" sur le bouton qui appelle l'eventListener
-            button.classList.add("active");
+            button.classList.add("active-btn");
 
             // met à jour la gallerie des travaux
             updateGallery(filteredWorks);
         });
     }
 }
-
-
 /**************************************************
- * Vérifie si l'utilisateur est connecté en vérifiant le localStorage
- * ajoute l'eventListener pour se connecter ou se déconnecter
+ * Vérifie si l'utilisateur est connecté
+ * en vérifiant le localStorage
+ * 
+ * @returns {boolean} "true" si l'admin est connecté
  **************************************************/
 function isAdminConnected() {
     const userId = window.localStorage.getItem("userId");
     const token = window.localStorage.getItem("token");
+    if (userId && token) return true; 
+}
+/**************************************************
+ * Change le menu Login en Logout
+ * et affiche le bouton "modifier" pour la gallerie
+ **************************************************/
+function ShowAdminFunctions() {
     const menuLogin = document.querySelector(".login");
-    
-    if (userId && token) {
-        menuLogin.innerText = "Logout";
-        const ancre = document.querySelector("#portfolio h2");
-        const admin = document.createElement("span");
-        admin.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> modifier`;
-        ancre.appendChild(admin);
-    } else {
-
-    }
-
-
+    menuLogin.innerText = "Logout";
+    // ajoute le bouton admin "modifier"
+    const element = document.querySelector("#portfolio h2");
+    const spanModifyBtn = `<button class="js-modal">
+                            <i class="fa-regular fa-pen-to-square"></i>modifier
+                            </button>`;
+    element.innerHTML += spanModifyBtn;
+}
+/**************************************************
+ * Ecoute si l'admin veut se déconnecter
+ **************************************************/
+function listenLogOut() {
+    const menuLogin = document.querySelector(".login");
     menuLogin.addEventListener("click", () => {
-        if (userId && token) {
-            // l'user veut se déconnecter
-            window.localStorage.setItem("userId", []);
-            window.localStorage.setItem("token", []);
-            window.location.reload();
-        } else {
-            // l'user veut se connecter
-            window.location.assign("./pages/login.html");
-        }
+        window.localStorage.setItem("userId", []);
+        window.localStorage.setItem("token", []);
+        window.location.reload();
+    });
+}
+/**************************************************
+ * Ecoute si on veut se connecter en tant qu'admin
+ **************************************************/
+function listenLogIn() {
+    const menuLogin = document.querySelector(".login");
+    menuLogin.addEventListener("click", () => {
+        window.location.assign("./pages/login.html");
     });
 }
 
+/**************************************************
+ * GESTION DES MODALES
+ **************************************************/
+let modal = null;
+const wrappers = document.querySelectorAll(".modal_wrapper");
+let activeWrapper = wrappers[0];
+activeWrapper.classList.add("activeWrapper");
+
+/**************************************************
+ * initialisation du EventListener pour la modale
+ **************************************************/
+function initAddEventListenerModal() {
+    document.querySelector(".js-modal").addEventListener("click", openModal);
+}
+/**************************************************
+ * Ouverture de la modale
+ **************************************************/
+const openModal = function (event) {
+    event.preventDefault();
+    modal = document.getElementById("modal");
+    modal.style.display = "flex";
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    showModalGallery();
+    modal.addEventListener("click", closeModal);
+    activeWrapper.querySelector(".js-modal-close").addEventListener("click", closeModal);
+}
+/**************************************************
+ * Fermeture de la modale
+ **************************************************/
+const closeModal = function (event) {
+    if (modal === null) return;
+    const jsBtnClose = activeWrapper.querySelector(".js-modal-close");
+    if (event.target === modal || event.target === jsBtnClose) {
+        modal.setAttribute("aria-hidden", "true");
+        modal.removeAttribute("aria-modal");
+        modal.removeEventListener("click", closeModal);
+        jsBtnClose.removeEventListener("click", closeModal);
+        modal.style.display = "none";
+        modal = null;
+    }
+}
+/**************************************************
+ * Sélection du wrapper à afficher
+ **************************************************/
+function switchWrapper(wrapper) {
+    // TODO => revoir la logique
+    if (wrapper === 0) { wrapper = 1; } else { wrapper = 0; }
+    for (let wrapperInList of wrappers) wrapperInList.classList.remove("activeWrapper");
+    wrappers[wrapper].classList.add("activeWrapper");
+}
+/**************************************************
+ * Récupère le filtrage actif
+ * et affichage les travaux dans la gallerie
+ **************************************************/
+function showModalGallery() {
+    const filtersbuttons = document.querySelectorAll(".filters button");
+    let category = "Tous"; // par défaut pour afficher tous les travaux
+    for (const button of filtersbuttons) {
+        if (button.classList.contains("active-btn")) category = button.dataset.category;
+    }
+    const filteredWorks = works.filter(function (work) {
+        return category === "Tous" ? true : work.category.name === category;        
+    });
+    
+    const gallery = document.querySelector(".galleryModal");
+    gallery.innerHTML = "";
+    for (const work of filteredWorks) {
+        const figure = `<figure>
+                            <img src="${work.imageUrl}" alt="${work.title}">
+                            <span class="trash" data-id="${work.id}"><i class="fa-solid fa-xs fa-trash-can"></i></span>
+                        </figure>`
+        gallery.innerHTML += figure;
+    }
+    addListenerToRemove();
+}
+/**************************************************
+ * Ajoute les listeners pour chaque image pour la demande de suppression
+ **************************************************/
+function addListenerToRemove() {
+    const worksList = modal.querySelectorAll(".galleryModal figure .trash");
+    for (const removeBtn of worksList) {
+        removeBtn.addEventListener("click", function () {
+            removeWork(removeBtn.dataset.id);
+        });
+    }
+}
+/**************************************************
+ * Supprime une image de la BDD
+ **************************************************/
+async function removeWork(workId) {
+    console.log("Ok pour supprimer l'image " + workId);
+    
+    
+    closeModal; // code à revoir pour aussi retirer les Listeners
+}
+/**************************************************
+ * FIN DES FONCTIONS
+ **************************************************/
 
 
+/**************************************************
+ * Début du Script
+ **************************************************/
 
+// génération des boutons de filtres dynamiquement
+addFilterButtons(listingCategories(works));
+// affichage de la gallery
+updateGallery(works);
+// ajout des Listeners sur les boutons filtres
+filteredButtonsListener();
+
+// si l'admin est connecté
+if (isAdminConnected()) {
+    // gère l'affichage si l'admin est connecté
+    ShowAdminFunctions();
+    // écoute les demandes de déconnexion
+    listenLogOut();
+    // écoute l'appel à la modale de modification 
+    initAddEventListenerModal();
+} else {
+    // écoute les demandes de connexion
+    listenLogIn();
+}
